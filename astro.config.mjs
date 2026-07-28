8b54775cb64588513795a2cb961fa978ea30219b
codefloe-admin <codefloe-admin@noreply.codefloe.com>
2026-07-28 12:24:31 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config'
-
 import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'   // add
 import Icons from 'unplugin-icons/vite'
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
   integrations: [vue()],
   vite: {
     plugins: [
-      // tailwindcss(),
-      Icons({
-        compiler: 'vue3',
-      }),
+      tailwindcss(),                            // uncomment/enable
+      Icons({ compiler: 'vue3' }),
     ],
   },
 })
