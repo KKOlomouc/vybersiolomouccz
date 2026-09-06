1eabb0c7c515207a875320ecfb0bc1bc39928432
Miloš Kroulík <milos.kroulik@hnutiduha.cz>
2026-09-06 21:24:47 +0200

    Add temp logo

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { nextTick } from 'vue'
 import { useStore, Stage } from '../store'
+import logo from "../assets/logos/vso-logo.webp";
 
 const props = defineProps<{ isApp: boolean }>()
 
@@ -20,7 +21,8 @@ const toIntro = (e: Event) => {
 <template>
   <header class="z-10 container mx-auto px-4 py-12">
     <a href="/" @click="toIntro">
-      <h1 class="mb-1 text-5xl font-bold md:text-8xl">Vyber si Olomouc</h1>
+      <h1 class="mb-3 text-5xl font-bold md:text-8xl">
+        <img class="aspect-auto object-cover" :src="logo.src" alt="Logo webu Vyber si Olomouc" width="528"></h1>
     </a>
     <span class="text-xl font-medium text-red-500">
       Volby do zastupitelstva města 2026
