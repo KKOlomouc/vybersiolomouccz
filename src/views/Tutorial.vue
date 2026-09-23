a3560fc5c23fc0c3b5f3b7edd7f271e09e348ed2
Miloš Kroulík <milos.kroulik@gmail.com>
2026-09-23 08:05:37 +0200

    Update tutorial

--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -8,10 +8,13 @@ const emit = defineEmits(['done'])
   <div class="bg-white p-4 md:p-8">
     <h2 class="text-lg!">Jak funguje web Vyber si Olomouc?</h2>
     <p>
-      Jde o předvolební kalkulačku - zaměřili jsme se jak na aktuální lokální kauzy, tak na témata, která odhalují hodnotové ukotvení kandidátstva.
+      Předvolební kompas je online nástroj, který porovnává vaše postoje ke konkrétním otázkám s postoji kandidujících. Srovnávají se odpovědi (ano, ne či nevím) na otázky, které se budou v následujícím volebním období v Olomouci řešit. Někteří z kandidátstva ke svým odpovědím přidali krátký komentář. Můžete si jej přečíst na stránce s vyhodnocením. Kompas tak nabízí další možnost, jak se před volbami seznámit s postoji kandidujících.</p>
+<p>Z každé kandidátní listiny jsme k vyplnění kompasu vyzvali prvních pět kandidujících. Za některá uskupení neodpověděl nikdo, za jiná jsme získali jednu či více odpovědí.</p>
+<p>Kompas nemá ambici postihnout názory všech kandidujících ani nahrazovat volební programy jednotlivých uskupení. Výsledky zachycují pouze postoje těch, kteří na dotazník odpověděli. Nástroj má voličkám a voličům nabídnout další zdroj informací, který mohou využít vedle volebních programů, veřejných debat a dalších dostupných informací.
+
     </p>
 
-    <h2 class="mt-4 text-lg!">Jak vyhodnocujeme shodu?</h2>
+    <!-- <h2 class="mt-4 text-lg!">Jak vyhodnocujeme shodu?</h2>
     <p>
       Eine Zustimmung entspricht der Option „ja, finde ich auch“. Eine Ablehnung
       und Enthaltung wird nach Begründung der Fraktion in Beschlussempfehlungen
@@ -43,8 +46,8 @@ const emit = defineEmits(['done'])
         sollen stärkere Maßnahmen ergriffen werden.
       </p>
       <p>
-          Někteří z kandidátstva ke svým odpovědím přidali krátký komentář. Můžete si jej přečíst na stránce s vyhodnocením.
-      </p>
+          
+      </p> -->
     </div>
 
     <button class="btn mt-4" @click="$emit('done')">
